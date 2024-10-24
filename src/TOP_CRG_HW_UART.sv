18d5a3589492a780988f541f89dc62e4813ba04c
ankoman <i08320.wctsonhb@gmail.com>
2024-10-24 19:01:20 +0900

    Add hardware sources

---
+++ src/TOP_CRG_HW_UART.sv
@@ -0,0 +1,22 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: AIST
+// Engineer: Junichi Sakamoto
+// 
+// Create Date: 2024/10/24
+// Module Name: TOP_CRG_HW_UART
+// Target Devices: Arty-A7(-100)
+// Tool Versions: Vivado 2020.1
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module TOP_CRG_HW_UART(
+    input   CLK100MHZ,
+            ck_rst_n,
+            uart_txd_in,
+    output  uart_rxd_out,
+    [3:0]   led,
+    );
+
+    
+endmodule
