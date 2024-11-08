ebdc40e56f20321565f22fcebe17ec6d055a0995
DESKTOP-2DVSNDA\sakamoto <i08320.wctsonhb@gmail.com>
2024-11-09 01:48:49 +0900

    temp

--- src/simd_muland.sv
+++ src/simd_muland.sv
@@ -24,7 +24,10 @@ module simd_muland
     );
 
     wire is_a = ~mode_i.b;
-    prng_t mask_in, w_x_in, w_y_in, r_x_in, r_y_in;
+    prng_t mask_in, w_x_in, w_y_in;
+    prng_t [7:0] w_acc_ps, w_acc_sc, r_acc_ps, r_acc_sc;
+    prng_t [8:0] r_x_in;
+    prng_split32_t[8:0] r_y_in;
     wire [31:0] mask32, mask64, mask128, mask256;
     prng_t [7:0] masks;
 
@@ -43,29 +46,52 @@ module simd_muland
         {mask32 , mask64 , mask128, mask128, mask256, mask256, mask256, mask256}
     };
 
+    parameter integer [7:0] tab_shifts [4] = {
+        {0,0,0,0,0,0,0,0}, // 32
+        {0, 32, 0, 32, 0, 32, 0, 32}, // 64
+        {0, 32, 64, 96, 0, 32, 64, 96}, // 128
+        {0, 32, 64, 96, 128, 160, 192, 224} // 256
+    };
 
     assign mask_in = {$bits(prng_t){is_a}};
     assign x_in = x_i & (y_i | mask_in);
     assign y_in = (y_i & mask_in) | {{($bits(prng_t) - 1){1'd0}}, 1'd1 ^ is_a};
+    assign w_acc_ps[0] = '0;
+    assign w_acc_sc[0] = '0;
 
-    //0th stage register.
+    //Pipeline stage registers.
     always_ff @(posedge clk_i) begin
         if(!rst_n_i) begin
             r_x_in <= '0;
             r_y_in <= '0;
+            r_acc_ps <= '0;
+            r_acc_sc <= '0;
         end
         else begin
-            r_x_in <= w_x_in;
-            r_y_in <= w_y_in;
+            r_x_in <= {r_x_in[7:0], w_x_in};
+            r_y_in <= {r_y_in[7:0], w_y_in};
+            r_acc_ps <= w_acc_ps;
+            r_acc_sc <= w_acc_sc;
         end
     end
 
-    u32_w_c_t [7:0][7:0] w_cy_sum, r_cy_sum;
-    u32_w_c_t [7:0] tmp_u32_w_c_t;
-
     generate
         for(genvar i = 0; i < 8; i = i + 1) begin
+            prng_t ps_o, sc_o;
+            CSAMUL_256_32 u_csamul(
+                .x_i(r_x_in[i]),
+                .y_i(r_y_in[i][i]),
+                .ps_o,
+                .sc_o
+            );
 
+            CSA #(.len($bits(prng_t))) u_csa_0 (
+                .a_i(),
+                .b_i(psc[i-1] << 1),
+                .c_i(pp),
+                .ps_o(pps[i]),
+                .sc_o(psc[i])
+            );
         end
     endgenerate
 
@@ -86,7 +112,7 @@ module CSAMUL_256_32
     generate
         for(genvar i = 0; i < 8; i = i + 1) begin
             prng_t pp;
-            assign pp = prng_t`((x_i[i] * y_i) << i*32);
+            assign pp = prng_t'((x_i[i] * y_i) << i*32);
 
             if (i == 0) begin
                 assign pps[0] = pp;
