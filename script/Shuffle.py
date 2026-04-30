bf902315c76550c57835f9875af72cac4b3bc8f2
Junichi Sakamoto <i08320.wctsonhb@gmail.com>
2026-04-30 13:37:11 +0900

    Refact

--- script/Shuffle.py
+++ script/Shuffle.py
@@ -3,8 +3,7 @@ import Crypto.Cipher.AES as AES
 from Crypto.Util import Counter
 
 N = 10
-PARTY_I = 0
-KEY = "1234567890abcdef1234567890abcdef"
+KEY = 0x1234567890abcdef1234567890abcdef
 
 class PRNG_128:
     def __init__(self, key, n_prefix):
@@ -26,18 +25,15 @@ class PRNG_128:
         else:
             ### 64 bit mode
             return (rnd >> 64) & 0xffffffffffffffff, rnd & 0xffffffffffffffff
-        
-def RPG(x: int):
-    return np.random.permutation(x)
 
 def PERM(dout, din, mode: int):
     if mode == 0:
         ### Permutation mode: dout <= din*dout
-        for i in range(N):
+        for i in range(len(dout)):
             dout[i] = din[dout[i]]
     elif mode == 1:
         ### Inverse mode
-        for i in range(N):
+        for i in range(len(dout)):
             dout[din[i]] = i
     else:
         raise ValueError("Invalid mode")
@@ -46,7 +42,7 @@ def ADD64(din1, din2, sub=False):
     dout = []
     MASK = 0xffffffffffffffff
 
-    for i in range(N):
+    for i in range(len(din1)):
         a = int(din1[i])
         b = int(din2[i])
 
@@ -57,70 +53,93 @@ def ADD64(din1, din2, sub=False):
 
     return dout
 
-### Init
-M1, M2, M3, M4, M5 = [np.arange(N) for _ in range(5)]
-AES1 = PRNG_128(key = bytes.fromhex(KEY), n_prefix = 0)
-AES2 = PRNG_128(key = bytes.fromhex(KEY), n_prefix = 1)
+class Shuffle:
+    def __init__(self, seed = 0, party = 0):
+        self.seed = seed
+        self.party = party
+        self.rng = np.random.default_rng(seed)
+        self.AES1 = PRNG_128(key=seed.to_bytes(16, 'big'), n_prefix=0)
+        self.AES2 = PRNG_128(key=seed.to_bytes(16, 'big'), n_prefix=1)
 
-### Step1
-M1 = RPG(N) # r
-save_M1 = M1.copy()
-print(f'r = {M1}')
-M2 = RPG(N) # r1
-M3 = RPG(N) # r1'
+    def RPG(self, n):
+        return self.rng.permutation(n)
+    
+    def LGA_prep(self, n):
+        ### Init
+        M1, M2, M3, M4, M5 = [np.arange(n) for _ in range(5)]
 
-### Step 2
-PERM(M4, M2, 1) # r1^-1
-PERM(M5, M3, 1) # r1'^-1
+        ### Step1
+        M1 = self.RPG(n) # r
+        save_r = M1.copy()
+        M2 = self.RPG(n) # r1
+        M3 = self.RPG(n) # r1'
 
-### Step 3
-PERM(M5, M1, 0) # M5 = r*r1'^-1=r2
-PERM(M1, M4, 0) # M4 = r1^-1*r=r2'
+        if self.party == 1:
+            ### Step 2
+            PERM(M4, M2, 1) # r1^-1
+            PERM(M5, M3, 1) # r1'^-1
 
-### Chech if r = r1r2' = r2r1'
-# PERM(M1, M2, 0) # r
-# PERM(M3, M5, 0) # r
-# print(M1)
-# print(M3)
+            ### Step 3
+            PERM(M5, M1, 0) # M5 = r*r1'^-1=r2
+            PERM(M1, M4, 0) # M4 = r1^-1*r=r2'
 
-### Step 4-2
-temp_a1 = []
-temp_a2 = []
-temp_c = []
+        ### Chech if r = r1r2' = r2r1'
+        # PERM(M1, M2, 0) # r
+        # PERM(M3, M5, 0) # r
+        # print(M1)
+        # print(M3)
+
+        ### Step 4-2
+        temp_a0 = []
+        temp_a1 = []
+        temp_c = []
+        for i in range(n):
+            temp = self.AES1.gen()
+            temp_a0.append(temp[0]) # a0
+            temp_a1.append(temp[1]) # a1
+            temp = self.AES2.gen()
+            temp_c.append(temp[0]) # c
+
+        temp_a0p = []
+        for i in range(n):
+            temp_a0p.append(temp_a1[M3[i]]) # a1r1'
+        temp_a0p = ADD64(temp_a0p, temp_c) # a0' = a1r1' + c
+
+        temp_a1p = []
+        for i in range(n):
+            temp_a1p.append(temp_a0[M1[i]]) # a0r2'
+        temp_a1p = ADD64(temp_a1p, temp_c, 1) # a1' = a0r2' - c
+
+        if self.party == 0:
+            return M2, M3, temp_a0, temp_a0p, save_r
+        else:
+            return M5, M1, temp_a1, temp_a1p
+        
+P0 = Shuffle(seed=KEY, party=0)
+P1 = Shuffle(seed=KEY, party=1)
+
+r0, r0p, a0, a0p, r = P0.LGA_prep(N)
+r1, r1p, a1, a1p = P1.LGA_prep(N)
+
+### LGA part
+v0 = ADD64(r0, a0)
+v1 = ADD64(r1, a1)
+
+y0 = []
 for i in range(N):
-    temp = AES1.gen()
-    temp_a1.append(temp[0]) # a1
-    temp_a2.append(temp[1]) # a2
-    temp = AES2.gen()
-    temp_c.append(temp[0]) # c
-
-temp_a1p = []
-for i in range(N):
-    temp_a1p.append(temp_a2[M3[i]]) # a2r1'
-temp_a1p = ADD64(temp_a1p, temp_c) # a1' = a2r1' + c
-
-temp_a2p = []
-for i in range(N):
-    temp_a2p.append(temp_a1[M1[i]]) # a1r2'
-temp_a2p = ADD64(temp_a2p, temp_c, 1) # a2' = a1r2' - c
-
-v1 = ADD64(M2, temp_a1)
-v2 = ADD64(M5, temp_a2)
+    y0.append(v1[r0p[i]]) # a1r0'
+y0 = ADD64(y0, a0p, 1) # y0
 
 y1 = []
 for i in range(N):
-    y1.append(v2[M3[i]]) # a2r1'
-y1 = ADD64(y1, temp_a1p, 1) # y1
+    y1.append(v0[r1p[i]]) # a1r2'
+y1 = ADD64(y1, a1p, 1) # y1
 
-y2 = []
-for i in range(N):
-    y2.append(v1[M1[i]]) # a1r2'
-y2 = ADD64(y2, temp_a2p, 1) # y2
-
-y = ADD64(y1, y2)
+y = ADD64(y0, y1)
+print(f'r = {r}')
 print(f'2r = {y}')
 
-if (save_M1 == np.array(y)//2).all():
+if (r == np.array(y)//2).all():
     print("Success!")
 else:    
     print("Failure...")
